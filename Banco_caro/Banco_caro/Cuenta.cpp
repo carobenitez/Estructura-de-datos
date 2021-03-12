9a45fc5c2c35066215c1906bc8a4b6ed44f9fd70
Carolina Benitez <78804879+carobenitez@users.noreply.github.com>
2021-03-12 10:16:03 -0400

    Add files via upload

---
+++ Banco_caro/Banco_caro/Cuenta.cpp
@@ -0,0 +1,29 @@
+#include "StdAfx.h"
+#include "Cuenta.h"
+
+
+Cuenta::Cuenta(void)
+{
+	saldo=0;
+}
+double Cuenta::get_saldo()
+{
+	return saldo;
+}
+void Cuenta::set_dinero(double dinero)
+{
+	saldo=dinero;  
+}
+void Cuenta::sacar(double dinero)
+	{
+		if(saldo>dinero)
+		{
+			saldo=saldo-dinero;
+		}else{
+			saldo=0;
+		}
+	}
+	void Cuenta::meter (double dinero)
+	{
+		saldo=saldo+dinero;
+	}
---
+++ Banco_caro/Banco_caro/Cuenta.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class Cuenta
+{ double saldo;
+public:
+	Cuenta(void);
+	double get_saldo();
+	void set_dinero(double dinero);
+	void sacar(double dinero);
+	void meter (double dinero);
+};
+
